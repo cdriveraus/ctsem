cc8ddd19e8733522b946c000c965e010b3a58977
cdriveraus <cdriver.aus@gmail.com>
2017-01-20 18:06:48 +0100

    fix ctIntervalise example...

--- R/ctIntervalise.R
+++ R/ctIntervalise.R
@@ -36,12 +36,12 @@
 #' data('longexample')
 #' 
 #' #Then convert to wide format
-#' wideexample <- ctLongToWide(datalong = longexample, id = "subject", 
-#' time = "Time", manifestNames = c("Y1", "Y2", "Y3"), 
+#' wideexample <- ctLongToWide(datalong = longexample, id = "id", 
+#' time = "time", manifestNames = c("Y1", "Y2", "Y3"), 
 #' TDpredNames = "TD1", TIpredNames = c("TI1", "TI2"))
 #' 
 #' #Then convert the absolute times to intervals, using the Tpoints reported from the prior step.
-#' wide <- ctIntervalise(datawide = wideexample, Tpoints = 3, n.manifest = 3, 
+#' wide <- ctIntervalise(datawide = wideexample, Tpoints = 4, n.manifest = 3, 
 #' n.TDpred = 1, n.TIpred = 2, manifestNames = c("Y1", "Y2", "Y3"), 
 #' TDpredNames = "TD1", TIpredNames = c("TI1", "TI2") )
 #'  
