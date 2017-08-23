6d34e48ab2c6fc76d299917dd5be29d2bcdee3cd
cdriveraus <cdriver.aus@gmail.com>
2017-08-23 17:54:52 +0200

    include ctStanParMatrices - get ct par matrices from raw params

---
+++ R/ctStanParMatrices.R
@@ -0,0 +1,120 @@
+#' Returns model equation matrices from a ctStanModel or ctStanFit, and vector of values for free parameters.
+#'
+#' @param model either a ctStanFit or ctStanModel object.
+#' @param parvalues vector of parameter values to assign to any free parameters in the model
+#' @param timeinterval time interval to use for discrete time (dt) matrix calculations.
+#'
+#' @return A list containing the various matrices used for computing the likelihood of a continuous time dynamic model.
+#' @export
+#'
+#' @examples
+ctStanParMatrices <- function(model, parvalues, timeinterval=1){
+  if(class(model)=='ctStanFit') model<- fit$ctstanmodel
+  if(class(model) !='ctStanModel') stop('not a ctStanModel')
+  
+  valuespec <- model$pars[is.na(model$pars$param),]
+  parspec <- model$pars[!is.na(model$pars$param),]
+  
+  if(length(parvalues)!=nrow(parspec)) stop('length of parvalues != number of free params in model!')
+  
+  covchol2corchol<-function(mat, invert){ 
+s=c()
+    o=mat;
+    
+    for(i in 1:nrow(o)){ 
+      for(j in min(i+1,nrow(mat)):nrow(mat)){
+        o[j,i] = inv_logit(o[j,i])*2-1;  # can change cor prior here
+        o[i,j] = o[j,i];
+      }
+      o[i,i]=1; #change to adjust prior for correlations
+    }
+    if(invert==1) o = solve(o);
+    
+    for(i in 1:nrow(o)){
+      s[i] = 1/sqrt(o[i,,drop=FALSE] %*% o[,i,drop=FALSE]);
+      if(is.infinite(s[i])) s[i]=0;
+    }
+    o= diag(s) %*% o;
+    return(o);
+  }
+  
+sdcovchol2cov <- function(mat, cholesky){ 
+    invert = 0; 
+    if(nrow(mat) > 1){
+      out=covchol2corchol(mat,invert); 
+      out= diag(diag(mat)) %*% out
+      }
+    if(nrow(mat)==1) out[1,1] = mat[1,1];
+    
+    if(cholesky==0) out = out %*% t(out);
+    return(out);
+  }
+  
+  
+  
+  for(mati in unique(model$pars$matrix)){
+    assign(mati,matrix(NA, 
+      nrow= max(model$pars$row[model$pars$matrix %in% mati]),
+      ncol= max(model$pars$col[model$pars$matrix %in% mati]),
+    ))
+  }
+
+  
+  for(i in 1:nrow(valuespec)){
+    eval(parse(text=paste0(valuespec$matrix[i], '[',valuespec$row[i],' ,', valuespec$col[i], '] <- ', valuespec$value[i])))
+  }
+  
+  for(i in 1:nrow(parspec)){
+    param <- parvalues[i]
+    eval(parse(text=paste0(parspec$matrix[i], '[',parspec$row[i],' ,', parspec$col[i], '] <- ', parspec$transform[i])))
+  }
+  
+DIFFUSION = sdcovchol2cov(DIFFUSION,0)
+T0VAR=sdcovchol2cov(T0VAR,0)
+MANIFESTVAR=MANIFESTVAR^2
+
+DRIFTHATCH<-DRIFT %x% diag(nrow(DRIFT)) + diag(nrow(DRIFT)) %x% DRIFT
+asymDIFFUSION<-matrix(-solve(DRIFTHATCH, c(DIFFUSION)), nrow=nrow(DRIFT))
+
+
+ln=model$latentNames
+mn=model$manifestNames
+tdn=model$TDpredNames
+
+dimnames(DRIFT)=list(ln,ln)
+dimnames(DIFFUSION)=list(ln,ln)
+dimnames(asymDIFFUSION)=list(ln,ln)
+rownames(CINT)=ln
+rownames(MANIFESTMEANS)=mn
+rownames(T0MEANS)=ln
+
+dimnames(T0VAR)=list(ln,ln)
+dimnames(asymDIFFUSION)=list(ln,ln)
+dimnames(LAMBDA)=list(mn,ln)
+
+
+dtDRIFT=expm(DRIFT * timeinterval)
+
+dtDIFFUSION = asymDIFFUSION - (dtDRIFT %*% asymDIFFUSION %*% t(dtDRIFT ))
+
+dtCINT = (solve(DRIFT) %*%(dtDRIFT - diag(2)) %*% (CINT))
+
+asymCINT = -solve(DRIFT) %*% CINT
+
+out<-list(DRIFT=DRIFT,dtDRIFT=dtDRIFT, T0VAR=T0VAR,DIFFUSION=DIFFUSION,dtDIFFUSION=dtDIFFUSION,
+  asymDIFFUSION=asymDIFFUSION,CINT=CINT,dtCINT=dtCINT, asymCINT=asymCINT, T0MEANS=T0MEANS,
+  MANIFESTMEANS=MANIFESTMEANS, LAMBDA=LAMBDA)
+
+if('MANIFESTVAR' %in% model$pars$matrix) {
+  dimnames(MANIFESTVAR)=list(mn,mn)
+  out$MANIFESTVAR=MANIFESTVAR
+  
+}
+
+if('TDPREDEFFECT' %in% model$pars$matrix) {
+  dimnames(TDPREDEFFECT)=list(ln,tdn)
+  out$TDPREDEFFECT<-TDPREDEFFECT
+}
+  
+return(out)
+}
