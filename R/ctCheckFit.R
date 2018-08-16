8621cbd08b414a2e11afe9c37a871b13ec1c50d3
cdriveraus <cdriver.aus@gmail.com>
2018-08-16 12:04:58 +0200

    ctCheckFit: fix max niter too high bug

--- R/ctCheckFit.R
+++ R/ctCheckFit.R
@@ -44,6 +44,7 @@ ctCheckFit <- function(fit, niter=500,probs=c(.025,.5,.975)){
   if(class(fit)=='ctStanFit'){
     e <- extract.ctStanFit(fit)
     ygendim <- dim(e$Ygen)
+    niter <- min(niter,ygendim[1])
     ygen <- array(e$Ygen,dim=c(ygendim[1] * ygendim[2],ygendim[-1:-2]))
     covarray<-array(NA,dim = c(dim(ecov),dim(ygen)[1]))
 
