6b0bf58a7ae84b4e30ab51220ec6a31303fa4554
cdriveraus <cdriver.aus@gmail.com>
2018-10-19 12:02:51 +0200

    import from KernSmooth

--- R/ctsem.R
+++ R/ctsem.R
@@ -46,6 +46,7 @@ utils::globalVariables(c("invDRIFT","II","DRIFTexp","vec2diag","diag2vec",
 #' @name ctsem
 #' @import grDevices methods stats rstan OpenMx graphics plyr rstantools rstan Rcpp DEoptim
 #' @importFrom utils relist as.relistable tail capture.output
+#' @importFrom KernSmooth bkde2d
 #' @useDynLib ctsem, .registration = TRUE
 #' 
 #' @references 
