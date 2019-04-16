0ef3ccc99e61a321e8692777d06cb632ef562941
cdriveraus <cdriver.aus@gmail.com>
2019-04-16 17:31:08 +0200

    listOfMatrices utility function for regenerating list of matrices from dataframe.

---
+++ R/listOfMatrices.R
@@ -0,0 +1,17 @@
+listOfMatrices <- function(df){
+  mats <- unique(df$matrix)
+  mlist <- list()
+  for(mi in mats){
+    mlist[[mi]] <- matrix(NA, max(df$row[df$matrix %in% mi]), max(df$col[df$matrix %in% mi]))
+    for(ci in 1:max(df$col[df$matrix %in% mi])){
+      for(ri in 1:max(df$row[df$matrix %in% mi])){
+        if(is.na(df$value[df$matrix %in% mi & df$row == ri & df$col==ci])) {
+          mlist[[mi]][ri,ci] <- df$param[df$matrix %in% mi & df$row == ri & df$col==ci]
+        } else {
+          mlist[[mi]][ri,ci] <- df$value[df$matrix %in% mi & df$row == ri & df$col==ci]
+        }
+      }
+    }
+  }
+  return(mlist)
+}
