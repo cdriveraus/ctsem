aeab48e979cc4ca5b4e753a0300dbb2b8f5bcb51
cdriveraus <cdriver.aus@gmail.com>
2019-09-27 13:25:09 +0200

    .

--- installctsem.R
+++ installctsem.R
@@ -1,3 +1,8 @@
+if(!is.null(names(sessionInfo()$otherPkgs))){ #unload packages
+  message('If any problems occur, please try this from a fresh R session')
+  suppressWarnings(invisible(lapply(paste0('package:', names(sessionInfo()$otherPkgs)), detach, character.only=TRUE, unload=TRUE)))
+}
+
 if(!requireNamespace('pkgbuild')) install.packages('pkgbuild')
 require(pkgbuild)
 try(pkgbuild::check_build_tools())
@@ -17,11 +22,9 @@ if(mv == 'N' || mv =='n'){ #create makevars
     if( grepl("^darwin", R.version$os)) "CXX14FLAGS += -arch x86_64 -ftemplate-depth-256" else
       if (.Platform$OS.type == "windows") "CXX11FLAGS=-O3 -mtune=native
 CXX14 = $(BINPREF)g++ -m$(WIN) -std=c++1y" else
-    "CXX14FLAGS += -fPIC",
+  "CXX14FLAGS += -fPIC",
     file = M, sep = "\n", append = TRUE)
 }
-if(!requireNamespace('rstan')) install.packages('rstan',dependencies = TRUE)
-if(!requireNamespace('OpenMx')) install.packages('OpenMx',dependencies = TRUE)
 
 if(.Platform$OS.type == "windows"){
   if(!suppressMessages(pkgbuild::has_rtools())) message('Waiting for Rtools installation to complete...')
@@ -29,6 +32,15 @@ if(.Platform$OS.type == "windows"){
     Sys.sleep(.1)
   }
 }
+
+old = old.packages()
+for(importantpack in c('rstan','OpenMx')){
+  if(importantpack %in% old) {
+    message('Updating ',importantpack)
+    install.packages(importantpack,dependencies = TRUE)
+  }
+}
+
 Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS='true')
 remotes::install_github('cdriveraus/ctsem', upgrade='never',INSTALL_opts = "--no-multiarch", 
   dependencies = c("Depends", "Imports"))
