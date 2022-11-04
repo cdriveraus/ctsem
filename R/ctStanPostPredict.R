ca172ccb0f3132661bfb869da7f7dd4eeb8ca0fd
Charles Driver <cdriver.aus@gmail.com>
2022-11-04 16:51:09 +0100

    ..

--- R/ctStanPostPredict.R
+++ R/ctStanPostPredict.R
@@ -18,8 +18,7 @@
 #' @return if plot=TRUE, nothing is returned and plots are created. Otherwise, a list containing ggplot objects is returned 
 #' and may be customized as desired.
 #' @examples
-#' if(w32chk()){
-#'
+#' \donttest{#'
 #' ctStanPostPredict(ctstantestfit,wait=FALSE, diffsize=2,resolution=100)
 #' }
 ctStanPostPredict <- function(fit,diffsize=1,jitter=.02, wait=TRUE,probs=c(.025,.5,.975),
