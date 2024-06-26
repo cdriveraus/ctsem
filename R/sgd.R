c31f68a0b73640606961d4fccbd7826b6a1cf739
Charles Driver <cdriver.aus@gmail.com>
2024-06-26 10:38:02 +0200

    ..

--- R/sgd.R
+++ R/sgd.R
@@ -205,8 +205,8 @@ sgd <- function(init,fitfunc,whichignore=c(),nsubsets=1,nsubjects=NA,plot=FALSE,
     deltaold=delta
     oldg=g
     g=attributes(lpg)$gradient
-    if(zeroG < 11){ #check zero gradient count
-      if(any(g %in% 0) && nsubsets == 1){
+    if(nsubsets == 1 && zeroG < 11){ #check zero gradient count
+      if(any(g %in% 0)){
         message(paste0('Gradient of parameter ',paste0(which(g %in% 0),collapse=', '), ' is exactly zero -- if this repeats or optimization fails, maybe model problem?'))
         zeroG <- zeroG + 1
         if(zeroG >  10) message ('Gradient warning limit exceeded, silencing...')
